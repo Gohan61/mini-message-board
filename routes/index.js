5841a4201c26f8019ef3b90fc4ed0c19548d489b
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-07-31 09:58:33 +0200

    Del: remove comment

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,6 @@ const express = require("express");
 const router = express.Router();
 const message_controlller = require("../controllers/MessageController");
 
-// const messages = [
-//   {
-//     text: "Hi there!",
-//     user: "Amando",
-//     added: new Date(),
-//   },
-//   {
-//     text: "Hello World!",
-//     user: "Charles",
-//     added: new Date(),
-//   },
-// ];
-
-/* GET home page. */
 router.get("/", message_controlller.getMessages);
 
 module.exports = router;
