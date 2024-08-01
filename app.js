5b98f1fe96fe4ad4012aaed6f540fbdaa069cc50
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-08-01 08:27:17 +0200

    Refactor: allow deployment to set own env var

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-const PORT = 3000;
-app.listen(PORT, () => console.log("Listening on PORT 3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 
 module.exports = app;
